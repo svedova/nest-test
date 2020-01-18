ccd4d2786baa2491bea561cba998b48d4e26768d
Savas Vedova <savas@vsk.me>
2020-01-18 20:43:20 +0100

    Init the app

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.init();
   return app.getHttpAdapter().getInstance();
 }
 
