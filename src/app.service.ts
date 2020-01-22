eb246ad632eb0dc21e9df1fec923b897641f1f57
Savas Vedova <savas@vsk.me>
2020-01-22 10:25:47 +0100

    Return an object

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Get, Header } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  @Get()
+  @Header('content-type', 'application/json')
+  getHello(): object {
+    return { "hello": "world" };
   }
 }
