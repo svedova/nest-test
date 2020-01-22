f39f2c60185fc70756ad82b03507722ba7853575
Savas Vedova <savas@vsk.me>
2020-01-22 13:08:49 +0100

    Use cors opts

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.init();
-
-  app.enableCors({
+  const cors = {
     origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type, Accept, X-Custom-Header",
-  });
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Accept, X-Custom-Header"
+  };
 
+  const app = await NestFactory.create(AppModule, { cors });
+  await app.init();
   return app.getHttpAdapter().getInstance();
 }
 
